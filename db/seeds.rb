b30d79d14972893267c45ddbcaff65feec879d53
a1810750 <a1810750@student.adelaide.edu.au>
2022-05-05 13:47:05 +0930

    "migrating database"

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,100 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+movies = Movie.create([{
+                         title: 'Soul',
+                         rating: 'G',
+                         description: 'After landing the gig of a lifetime, a New York jazz pianist suddenly finds himself trapped in a strange land between Earth and the afterlife.',
+                         release_date: '11/10/2022',
+                         created_at: '11/10/2022' ,
+                         updated_at: '11/10/2022'
+                       },
+                       {
+                         title: 'The Lord of the Rings: The Fellowship of the Ring',
+                         rating: 'PG',
+                         description: 'The Fellowship of the Ring embark on a journey to destroy the One Ring and end Saurons reign over Middle-earth. A young Hobbit known as Frodo has been thrown on an amazing adventure, when he is appointed the job of destroying the One Ring, which was created by the Dark Lord Sauron.',
+                         release_date: '8/10/2009',
+                         created_at: '11/10/2022' ,
+                         updated_at: '11/10/2022'
+                       },
+                       {
+                         title: 'Encanto',
+                         rating: 'G',
+                         description: 'The Madrigals are an extraordinary family who live hidden in the mountains of Colombia in a charmed place called the Encanto.',
+                         release_date: '2/1/2022',
+                         created_at: '11/10/2022' ,
+                         updated_at: '11/10/2022'
+                       },
+                       {
+                         title: 'Moana',
+                         rating: 'G',
+                         description: 'Moana, daughter of chief Tui, embarks on a journey to return the heart of goddess Te Fitti from Maui, a demigod, after the plants and the fish on her island start dying due to a blight.',
+                         release_date: '23/11/2016',
+                         created_at: '11/10/2022' ,
+                         updated_at: '11/10/2022'
+                       },
+                       {
+                         title: 'Mulan',
+                         rating: 'PG',
+                         description: 'Young Mulan is distraught to learn that her weak father must join the army to fight the invading Huns. Unwilling to endanger his life, she disguises herself as a man and joins the army in his place.',
+                         release_date: '5/6/1989',
+                         created_at: '11/10/2022' ,
+                         updated_at: '11/10/2022'
+                       },
+                       {
+                         title: 'Saw',
+                         rating: 'PG-13',
+                         description: 'Saw is a 2004 American horror film directed by James Wan, in his feature directorial debut, and written by Leigh Whannell from a story by Wan and Whannell.',
+                         release_date: '5/6/2004',
+                         created_at: '11/10/2022' ,
+                         updated_at: '11/10/2022'
+                       },
+                       {
+                         title: 'Saw II',
+                         rating: 'PG-13',
+                         description: 'Saw II is a 2005 horror film directed by Darren Lynn Bousman and written by Leigh Whannell and Bousman. It is the sequel to 2004s Saw and the second installment in the Saw film series.',
+                         release_date: '2/1/2005',
+                         created_at: '11/10/2022' ,
+                         updated_at: '11/10/2022'
+                       },
+                       {
+                         title: 'Tangled',
+                         rating: 'G',
+                         description: 'Rapunzel, a naive and young girl, is locked up by her overly protective mother',
+                         release_date: '9/9/2010',
+                         created_at: '11/10/2022' ,
+                         updated_at: '11/10/2022'
+                       },
+                       {
+                         title: 'Spirited Away',
+                         rating: 'PG-13',
+                         description: 'Ten-year-old Chihiro and her parents end up at an abandoned amusement park inhabited by supernatural beings.',
+                         release_date: '12/12/2002',
+                         created_at: '11/10/2022' ,
+                         updated_at: '11/10/2022'
+                       },
+                       {
+                         title: 'Howels Moving Castle',
+                         rating: 'PG-13',
+                         description: 'Jealous of Sophies closeness to Howl, a wizard, the Witch of Waste transforms her into an old lady.',
+                         release_date: '22/8/2005',
+                         created_at: '11/10/2022' ,
+                         updated_at: '11/10/2022'
+                       },
+                       {
+                         title: 'One Direction: Live on Tour',
+                         rating: 'PG-13',
+                         description: 'Up All Night: The Live Tour is a video album documenting the 3 January 2012 show of English-Irish boy band One Directions Up All Night Tour. ',
+                         release_date: '3/1/2011',
+                         created_at: '11/10/2022' ,
+                         updated_at: '11/10/2022'
+                       },
+                       {
+                         title: 'Nightmare on elm street',
+                         rating: 'R',
+                         description: 'A young girl realises that she has to stay awake at any cost to avoid the clawed killer who is butchering her friends one by one in their dreams.',
+                         release_date: '2/1/1998',
+                         created_at: '11/10/2022' ,
+                         updated_at: '11/10/2022'
+                       },
+                      ])
