731369a1304b40ac0944a31dde00b0e24356991b
a1810750 <a1810750@student.adelaide.edu.au>
2022-05-05 14:14:13 +0930

    "adding directors"

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,15 +11,18 @@ movies = Movie.create([{
                          description: 'After landing the gig of a lifetime, a New York jazz pianist suddenly finds himself trapped in a strange land between Earth and the afterlife.',
                          release_date: '11/10/2022',
                          created_at: '11/10/2022' ,
-                         updated_at: '11/10/2022'
-                       }, 
+                         updated_at: '11/10/2022',
+                         director: 'Pete Docter'
+
+                       },
                        {
                          title: 'The Lord of the Rings: The Fellowship of the Ring',
                          rating: 'PG',
                          description: 'The Fellowship of the Ring embark on a journey to destroy the One Ring and end Saurons reign over Middle-earth. A young Hobbit known as Frodo has been thrown on an amazing adventure, when he is appointed the job of destroying the One Ring, which was created by the Dark Lord Sauron.',
                          release_date: '8/10/2009',
                          created_at: '11/10/2022' ,
-                         updated_at: '11/10/2022'
+                         updated_at: '11/10/2022',
+                         director: 'Peter Jackson'
                        },
                        {
                          title: 'Encanto',
@@ -27,7 +30,8 @@ movies = Movie.create([{
                          description: 'The Madrigals are an extraordinary family who live hidden in the mountains of Colombia in a charmed place called the Encanto.',
                          release_date: '2/1/2022',
                          created_at: '11/10/2022' ,
-                         updated_at: '11/10/2022'
+                         updated_at: '11/10/2022',
+                         director: 'Byron Howard'
                        },
                        {
                          title: 'Moana',
@@ -35,7 +39,8 @@ movies = Movie.create([{
                          description: 'Moana, daughter of chief Tui, embarks on a journey to return the heart of goddess Te Fitti from Maui, a demigod, after the plants and the fish on her island start dying due to a blight.',
                          release_date: '23/11/2016',
                          created_at: '11/10/2022' ,
-                         updated_at: '11/10/2022'
+                         updated_at: '11/10/2022',
+                         director: 'Ron Clements'
                        },
                        {
                          title: 'Mulan',
@@ -43,7 +48,8 @@ movies = Movie.create([{
                          description: 'Young Mulan is distraught to learn that her weak father must join the army to fight the invading Huns. Unwilling to endanger his life, she disguises herself as a man and joins the army in his place.',
                          release_date: '5/6/1989',
                          created_at: '11/10/2022' ,
-                         updated_at: '11/10/2022'
+                         updated_at: '11/10/2022',
+                         director: 'Tony Bancroft'
                        },
                        {
                          title: 'Saw',
@@ -51,7 +57,8 @@ movies = Movie.create([{
                          description: 'Saw is a 2004 American horror film directed by James Wan, in his feature directorial debut, and written by Leigh Whannell from a story by Wan and Whannell.',
                          release_date: '5/6/2004',
                          created_at: '11/10/2022' ,
-                         updated_at: '11/10/2022'
+                         updated_at: '11/10/2022',
+                         director: 'James Wan'
                        },
                        {
                          title: 'Saw II',
@@ -59,7 +66,8 @@ movies = Movie.create([{
                          description: 'Saw II is a 2005 horror film directed by Darren Lynn Bousman and written by Leigh Whannell and Bousman. It is the sequel to 2004s Saw and the second installment in the Saw film series.',
                          release_date: '2/1/2005',
                          created_at: '11/10/2022' ,
-                         updated_at: '11/10/2022'
+                         updated_at: '11/10/2022',
+                         director: 'Darren Lynn Bousman'
                        },
                        {
                          title: 'Tangled',
@@ -67,7 +75,8 @@ movies = Movie.create([{
                          description: 'Rapunzel, a naive and young girl, is locked up by her overly protective mother',
                          release_date: '9/9/2010',
                          created_at: '11/10/2022' ,
-                         updated_at: '11/10/2022'
+                         updated_at: '11/10/2022',
+                         director: 'Nathan Greno'
                        },
                        {
                          title: 'Spirited Away',
@@ -75,7 +84,8 @@ movies = Movie.create([{
                          description: 'Ten-year-old Chihiro and her parents end up at an abandoned amusement park inhabited by supernatural beings.',
                          release_date: '12/12/2002',
                          created_at: '11/10/2022' ,
-                         updated_at: '11/10/2022'
+                         updated_at: '11/10/2022',
+                         director: 'Hayao Miyazaki'
                        },
                        {
                          title: 'Howels Moving Castle',
@@ -83,7 +93,8 @@ movies = Movie.create([{
                          description: 'Jealous of Sophies closeness to Howl, a wizard, the Witch of Waste transforms her into an old lady.',
                          release_date: '22/8/2005',
                          created_at: '11/10/2022' ,
-                         updated_at: '11/10/2022'
+                         updated_at: '11/10/2022',
+                         director: 'Hayao Miyazaki'
                        },
                        {
                          title: 'One Direction: Live on Tour',
@@ -91,7 +102,8 @@ movies = Movie.create([{
                          description: 'Up All Night: The Live Tour is a video album documenting the 3 January 2012 show of English-Irish boy band One Directions Up All Night Tour. ',
                          release_date: '3/1/2011',
                          created_at: '11/10/2022' ,
-                         updated_at: '11/10/2022'
+                         updated_at: '11/10/2022',
+                         director: 'Andy Saunders'
                        },
                        {
                          title: 'Nightmare on elm street',
@@ -99,6 +111,7 @@ movies = Movie.create([{
                          description: 'A young girl realises that she has to stay awake at any cost to avoid the clawed killer who is butchering her friends one by one in their dreams.',
                          release_date: '2/1/1998',
                          created_at: '11/10/2022' ,
-                         updated_at: '11/10/2022'
+                         updated_at: '11/10/2022',
+                         director: 'Wes Craven'
                        },
                       ])
