911f819d331e5dea0625c5f164fcf81759e140c1
a1810750 <a1810750@student.adelaide.edu.au>
2022-05-05 14:27:49 +0930

    "reseeding database"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_110923) do
+ActiveRecord::Schema.define(version: 2022_05_05_043634) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_03_22_110923) do
     t.date "release_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "director"
   end
 
 end
